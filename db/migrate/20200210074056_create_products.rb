6af5e22eed21c592295506550ecfb7f8e19b807e
wish13t <vishal13joshi@gmail.com>
2020-02-10 15:53:25 +0530

    Scaffolding Products

---
+++ db/migrate/20200210074056_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :tittle
+      t.string :author
+      t.string :description
+      t.string :image_url
+      t.decimal :price, precision: 8, scale: 3
+
+      t.timestamps
+    end
+  end
+end
